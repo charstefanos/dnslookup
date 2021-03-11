fb600430a57b55d66642fefa10e9327c32b753a8
charstefanos <charstefanos@gmail.com>
2021-03-11 09:15:33 +0000

    Update dnslookup.c

--- dnslookup.c
+++ dnslookup.c
@@ -1,10 +1,6 @@
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <netdb.h>
 
 #define BUFLEN 40
